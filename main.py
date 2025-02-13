f6d24361dd1854dd6aea63ae0ea6898743dd268c
Vishal Adithya.A <vishaladithya2008@gmail.com>
2025-02-13 15:20:38 +0530

    Defined a function for feature extraction

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 
 # removing unecessary files
 folder = "Data/"
+
 for file in os.listdir(folder):
     if file.endswith(".txt"):
         os.remove(os.path.join(folder,file))
@@ -17,7 +18,7 @@ plt.title("reference image BGR")
 plt.show()
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-plt.imshow(img,cmap = "gray")
+plt.imshow(gray,cmap = "gray")
 plt.title("reference image GRAY")
 plt.show()
 
@@ -27,4 +28,32 @@ def Preprocessing(img_path,img_size = (256,256)):
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img,gray
 
+# feature extraction
+height,width = gray.shape
+x_grid,y_grid = np.meshgrid(np.linspace(0,1,width),np.linspace(0, 1,height))
+gray_flat = gray.flatten()
+x_grid_flat = x_grid.flatten()
+y_grid_flat = y_grid.flatten()
+
+plt.imshow(x_grid)
+plt.title("X Grid")
+plt.show()
+plt.imshow(y_grid)
+plt.title("Y Grid")
+plt.show()
+
+def Feature_Extraction(gray_img):
+    height,width = gray_img.shape
+    x,y = np.meshgrid(np.linspace(0, 1,width),np.linspace(0, 1,height))
+    gray_img_flat = gray_img.flatten()
+    x_falt = x.flatten()
+    y_flat = y.flatten()
+    
+    feature_stack = np.column_stack((gray_img_flat,x_falt,y_flat))
+    return feature_stack
+
+y_r = []
+y_g = []
+y_b = []
+X_data = []
 
